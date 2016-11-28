16e885717766386739e98ebf2a8fff82901036bb
xujian <xujian_xtkf@win-sky.com.cn>
2016-11-28 15:22:56 +0800

    Rise input handler capacity

--- ProjectManagementSystemChecker/src/main/java/com/flag/xu/project/system/AnnotationTestMain.java
+++ ProjectManagementSystemChecker/src/main/java/com/flag/xu/project/system/AnnotationTestMain.java
@@ -5,6 +5,8 @@ import com.flag.xu.project.system.pojo.XmlTestPojo;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Authuor Administrator
@@ -12,22 +14,28 @@ import java.lang.reflect.Method;
  */
 public class AnnotationTestMain {
     public static void main(String[] args) {
-        Class<XmlTestPojo> testPojoClass = XmlTestPojo.class;
-        Field[] fields = testPojoClass.getDeclaredFields();
-        Method[] methods = testPojoClass.getDeclaredMethods();
-        for (Field field : fields){
-            if (field.isAnnotationPresent(PropertyIgnore.class))
-                continue;
-            else
-                System.out.println(field.getName());
-        }
-
-        for (Method method : methods){
-            if (method.isAnnotationPresent(PropertyIgnore.class))
-                continue;
-            else
-                System.out.println(method.getName());
-        }
+//        Class<XmlTestPojo> testPojoClass = XmlTestPojo.class;
+//        Field[] fields = testPojoClass.getDeclaredFields();
+//        Method[] methods = testPojoClass.getDeclaredMethods();
+//        for (Field field : fields){
+//            if (field.isAnnotationPresent(PropertyIgnore.class))
+//                continue;
+//            else
+//                System.out.println(field.getName());
+//        }
+//
+//        for (Method method : methods){
+//            if (method.isAnnotationPresent(PropertyIgnore.class))
+//                continue;
+//            else
+//                System.out.println(method.getName());
+//        }
+        String[] strings = new String[]{"hello", "world"};
+        List<String> list = Arrays.asList(strings);
+        String string = "hello,world";
+        System.out.println(string.contains("hello,world"));
+        System.out.println(string.contains(""));
+        list.forEach(System.out::println);
     }
 
 }
