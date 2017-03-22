e4925fce9e543653ab8518d4fe6c91cba99fa480
xujian <xujian_xtkf@win-sky.com.cn>
2017-03-22 17:19:08 +0800

    add module, for some hbase test

---
+++ neko-hbase/src/main/java/com/flag/xu/neko/hbase/HbaseMain.java
@@ -0,0 +1,11 @@
+package com.flag.xu.neko.hbase;
+
+/**
+ * main class
+ *
+ * @author xuj
+ * @version V1.0-SNAPSHOT
+ * @since 2017-03-22-17:14
+ */
+public class HbaseMain {
+}
---
+++ neko-hbase/src/main/java/com/flag/xu/neko/hbase/repo/TableRepository.java
@@ -0,0 +1,11 @@
+package com.flag.xu.neko.hbase.repo;
+
+/**
+ * hbase table repository
+ *
+ * @author xuj
+ * @version V1.0-SNAPSHOT
+ * @since 2017-03-22-17:17
+ */
+public class TableRepository {
+}
