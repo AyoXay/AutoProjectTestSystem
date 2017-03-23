baf298c839c679a116f7848722b641a8d0659183
xujian <xujian_xtkf@win-sky.com.cn>
2017-03-23 18:00:27 +0800

    method start

--- neko-hbase/src/main/java/com/flag/xu/neko/hbase/repo/TableRepository.java
+++ neko-hbase/src/main/java/com/flag/xu/neko/hbase/repo/TableRepository.java
@@ -1,5 +1,10 @@
 package com.flag.xu.neko.hbase.repo;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * hbase table repository
  *
@@ -8,4 +13,16 @@ package com.flag.xu.neko.hbase.repo;
  * @since 2017-03-22-17:17
  */
 public class TableRepository {
+
+    private static final Logger LOG = LogManager.getLogger(TableRepository.class);
+
+    private static final String TABLE_NAME = "million_row";
+
+    private static final String CF_DEFAULT = "cf";
+
+    private Configuration conf = HBaseConfiguration.create();
+
+    public void saveObjToTable(Object obj){
+
+    }
 }
